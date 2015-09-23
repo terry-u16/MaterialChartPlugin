0b63b0a0e92bc71a4f4a412afcc9a7fa2276166d
terry-u16 <gray_labo@hotmail.co.jp>
2015-09-23 13:14:28 +0900

    [Modify] Improve x-axis algorithm
    - Add code to get x-axis intervals manually.

---
+++ MaterialChartPlugin/Views/Converters/DisplayedPeriodToIntervalConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.DataVisualization.Charting;
+using System.Windows.Data;
+using MaterialChartPlugin.Models;
+
+namespace MaterialChartPlugin.Views.Converters
+{
+    public class DisplayedPeriodToIntervalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var period = (DisplayedPeriod)value;
+
+            switch (period)
+            {
+                case DisplayedPeriod.OneDay:
+                    return 6.0;
+                case DisplayedPeriod.OneWeek:
+                    return 1.0;
+                case DisplayedPeriod.OneMonth:
+                    return 1.0;
+                case DisplayedPeriod.ThreeMonths:
+                    return 2.0;
+                case DisplayedPeriod.OneYear:
+                    return 3.0;
+                case DisplayedPeriod.ThreeYears:
+                    return 6.0;
+                default:
+                    throw new ArgumentException("periodの値が不正です");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MaterialChartPlugin/Views/Converters/DisplayedPeriodToIntervalTypeConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.DataVisualization.Charting;
+using System.Windows.Data;
+using MaterialChartPlugin.Models;
+
+namespace MaterialChartPlugin.Views.Converters
+{
+    public class DisplayedPeriodToIntervalTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var period = (DisplayedPeriod)value;
+
+            switch (period)
+            {
+                case DisplayedPeriod.OneDay:
+                    return DateTimeIntervalType.Hours;
+                case DisplayedPeriod.OneWeek:
+                    return DateTimeIntervalType.Days;
+                case DisplayedPeriod.OneMonth:
+                    return DateTimeIntervalType.Weeks;
+                case DisplayedPeriod.ThreeMonths:
+                    return DateTimeIntervalType.Weeks;
+                case DisplayedPeriod.OneYear:
+                    return DateTimeIntervalType.Months;
+                case DisplayedPeriod.ThreeYears:
+                    return DateTimeIntervalType.Months;
+                default:
+                    throw new ArgumentException("periodの値が不正です");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
