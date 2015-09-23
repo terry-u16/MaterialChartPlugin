9d6e1f630c48771f7011908e182efe76e2792d25
terry-u16 <gray_labo@hotmail.co.jp>
2015-09-23 20:50:31 +0900

    [Upgrade] version 1.0.0
    - Update the plugin version.

--- MaterialChartPlugin/Properties/AssemblyInfo.cs
+++ MaterialChartPlugin/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MaterialChartPlugin")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2015 terry_u16")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
