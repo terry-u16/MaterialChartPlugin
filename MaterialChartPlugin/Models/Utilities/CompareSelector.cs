242e1ea7b28e841d6860c5060286caf332700243
terry-u16 <gray_labo@hotmail.co.jp>
2015-11-01 18:53:01 +0900

    [Add] IEnumerable.Distinctの拡張メソッドを追加
    - 汎用CompareSelectorの追加
    - IEnumerable.Distinctの拡張メソッドを追加

---
+++ MaterialChartPlugin/Models/Utilities/CompareSelector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialChartPlugin.Models.Utilities
+{
+    /// <summary>
+    /// 汎用のComparer（http://neue.cc/2009/08/07_184.html を参照）
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TKey"></typeparam>
+    public class CompareSelector<T, TKey> : IEqualityComparer<T>
+    {
+        private Func<T, TKey> selector;
+
+        public CompareSelector(Func<T, TKey> selector)
+        {
+            this.selector = selector;
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return selector(x).Equals(selector(y));
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return selector(obj).GetHashCode();
+        }
+    }
+}
---
+++ MaterialChartPlugin/Models/Utilities/LinqExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialChartPlugin.Models.Utilities
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<T> Distinct<T, TKey>(this IEnumerable<T> source, Func<T, TKey> selector)
+        {
+            return source.Distinct(new CompareSelector<T, TKey>(selector));
+        }
+    }
+}
